9eb6af9b08008624b69b76cf37f90a9a5a856e0a
Leonardo Chávez <n00449821pn.pe>
2026-06-30 02:11:03 -0500

    Creacion de interfaz

--- Ejecucion/Program.cs
+++ Ejecucion/Program.cs
@@ -11,7 +11,38 @@ namespace Ejecucion
     {
         static void Main(string[] args)
         {
-            Grafo gf=new Grafo();
+            //Grafo gf=new Grafo(5); // Por ejemplo, 5 ciudades
+            int op = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\t\t========HABITTRACK========");
+                Console.WriteLine("1. Ingrese cantidad de lugares: ");
+                Console.WriteLine("2. Generar matriz de adyacencia");
+                Console.WriteLine("3. Calcular ruta más corta");
+                Console.WriteLine("0. Salir");
+                Console.Write("INGRESE UNA OPCIÓN: ");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        Console.Write("Ingrese la cantidad de lugares: ");
+                        int cant = int.Parse(Console.ReadLine());
+                        Grafo gf = new Grafo(cant);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+
+                    default:
+                        Console.WriteLine("Ingrese una opción válida.");
+                        break;
+                }
+                Console.ReadKey();  
+            } while (op!=0);
+
 
         }
     }
