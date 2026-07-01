482da04002920958b62da0bdae69bc6632407f7b
Leonardo Chávez <n00449821pn.pe>
2026-07-01 02:13:42 -0500

    program hecho

--- Clases/Pila.cs
+++ Clases/Pila.cs
@@ -8,5 +8,39 @@ namespace Clases
 {
     public class Pila
     {
+        public Nodo cima = null;
+
+        //apilar(PUSH)
+        public void Apilar(string nombreLugar)
+        {
+            //1. crear nodo
+            Nodo nuevo = new Nodo();
+            nuevo.dato = nombreLugar;
+            /*reducir código
+            nuevo.siguiente = cima;
+            cima = nuevo;*/
+            if (cima == null)
+            {
+                cima = nuevo;
+            }
+            else
+            {
+                nuevo.siguiente = cima;
+                cima = nuevo;
+            }
+        }
+
+        //Desapilar
+        public string Desapilar()
+        {
+            string dato = null;
+            if (cima != null)
+            {
+                dato = cima.dato;
+                cima = cima.siguiente;
+            }
+            return dato;
+        }
+
     }
 }
--- Clases/Vertice.cs
+++ Clases/Vertice.cs
@@ -9,6 +9,7 @@ namespace Clases
     public class Vertice
     {
         public Lugar dato;
+        public 
         public Vertice sig=null;
 
         public ListaAristas ls = new ListaAristas();
